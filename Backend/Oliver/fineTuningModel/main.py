540bb3d5d1d84bc9e292f2368be7aa3f0819b78b
Oliver <olth@itu.dk>
2024-03-21 09:57:05 +0100

    Change file structure + refactor

--- Backend/Oliver/fineTuningModel/main.py
+++ Backend/Oliver/fineTuningModel/main.py
@@ -9,20 +9,23 @@ API_KEY = "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"
 current_directory = os.path.dirname(__file__)
 
 # Default file paths
-excel_questions_filepath = os.path.join(current_directory, "data", "questions.xlsx")
-csv_interviews_filepath = os.path.join(current_directory, "data", "interviews.csv")
+excel_questions_filepath = os.path.join(current_directory, "data", "questions", "questions.xlsx")
+csv_interviews_filepath = os.path.join(current_directory, "data", "interviews", "interviews.csv")
 
 # Created file paths
-csv_questions_filepath = os.path.join(current_directory, "data", "questions.csv")
-json_questions_filepath = os.path.join(current_directory, "data", "questions.jsonl")
-training_questions_filepath = os.path.join(current_directory, "data", "training_set_questions.jsonl")
-validation_questions_filepath = os.path.join(current_directory, "data", "validation_set_questions.jsonl")
+csv_questions_filepath = os.path.join(current_directory, "data", "questions", "questions.csv")
+json_questions_filepath = os.path.join(current_directory, "data", "questions", "questions.jsonl")
+training_questions_filepath = os.path.join(current_directory, "data", "questions", "training_set_questions.jsonl")
+validation_questions_filepath = os.path.join(current_directory, "data", "questions", "validation_set_questions.jsonl")
 
-json_interviews_synonym_filepath = os.path.join(current_directory, "data", "interview_synonym.jsonl")
+json_interviews_filepath = os.path.join(current_directory, "data", "interviews", "interviews.jsonl")
+training_interviews_filepath = os.path.join(current_directory, "data", "interviews", "training_set_interviews.jsonl")
+validation_interviews_filepath = os.path.join(current_directory, "data", "interviews", "validation_set_interviews.jsonl")
+json_interviews_synonym_filepath = os.path.join(current_directory, "data", "interviews", "interview_synonym.jsonl")
 
-#%% Create datasets
+#%% Create datasets for questions
 
-from createDatasets import df_to_csv, format_to_chatgpt_format, train_and_val_set
+from Backend.Oliver.fineTuningModel.scripts.createDatasets import df_to_csv, format_to_chatgpt_format, train_and_val_set
 import pandas as pd
 
 # Convert Excel dataset to DataFrame
@@ -37,27 +40,60 @@ format_to_chatgpt_format(dataset_df_with_headers, json_questions_filepath)
 # Split dataset into training and validation sets
 train_and_val_set(dataset_df_with_headers, training_questions_filepath, validation_questions_filepath)
 
-#%% Tune the model
+#%% Tune model for questions
 
-from finetuning import tune_data
+from Backend.Oliver.fineTuningModel.scripts.finetuning import tune_data
 
 tune_data(training_questions_filepath, validation_questions_filepath, API_KEY)
 
-#%% Test the model
+#%% Test model for questions
 
-from model import test_model
+from Backend.Oliver.fineTuningModel.scripts.model import test_model
 
 phrase = "har du nogensinde fået metalsplinter i øjet?"
 test_model(phrase)
 
-#%% Validate training set
+#%% Validate training set for questions
 
-from validating import validate_data
+from Backend.Oliver.fineTuningModel.scripts.validating import validate_data
 
 validate_data(training_questions_filepath)
 
-#%% Augment dataset
+#%% Create datasets for interviews
 
-from augmentation import backtranslate_augmenter, char_swap_augmenter, embed_and_translate
+from Backend.Oliver.fineTuningModel.scripts.createDatasets import format_to_chatgpt_format, train_and_val_set
+import pandas as pd
 
-embed_and_translate(csv_interviews_filepath, json_interviews_synonym_filepath)   
+dataset_df = pd.read_csv(csv_interviews_filepath)
+
+format_to_chatgpt_format(dataset_df, json_interviews_filepath)
+
+train_and_val_set(dataset_df, training_interviews_filepath, validation_interviews_filepath)
+
+#%% Augment interview dataset
+
+from Backend.Oliver.fineTuningModel.scripts.augmentation import backtranslate_augmenter, char_swap_augmenter, embed_and_translate
+
+embed_and_translate(csv_interviews_filepath, json_interviews_synonym_filepath)
+
+#%% Hyperparameter tuning
+
+from Backend.Oliver.fineTuningModel.scripts.hyperparameter_tuning import generate_translations, save_as_csv, calculate_combined_score, create_hyperparameter_combinations, fine_tune_models
+
+FT_model_ids = {
+    "InterviewOnly": "ft:gpt-3.5-turbo-0125:personal:medilingo:8z7ujSsh"
+}
+
+best_model = {
+    "InterviewOnly": "ft:gpt-3.5-turbo-0125:personal:medilingo:8z7ujSsh"
+}
+
+create_hyperparameter_combinations(API_KEY, training_interviews_filepath, validation_interviews_filepath)
+
+fine_tune_models(API_KEY)
+
+calculate_combined_score(API_KEY, FT_model_ids)
+
+generated = generate_translations(API_KEY, best_model["InterviewOnly"])
+
+save_as_csv(generated)
